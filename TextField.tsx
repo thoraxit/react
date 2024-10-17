2163b9eedd72bc2c360ee6694cb4ad0579369b09
Thorax IT <118170367+thoraxit@users.noreply.github.com>
2024-10-18 03:59:19 +0600

    Update TextField.tsx

--- TextField.tsx
+++ TextField.tsx
@@ -1,1 +1,19 @@
-// Text Field
+                      <TextField
+                            className="w-[300px]"
+                            {...register('billNo')}
+                            label="Bill No"
+                            error={!!errors.billNo}
+                            helperText={errors.billNo?.message}
+                            margin="normal"
+                            autoComplete="off"
+                            sx={{
+                                marginRight: '8px',
+                                '& .MuiInputBase-input': {
+                                    fontWeight: 'bold',
+                                },
+                                '& .MuiInputLabel-root': {
+
+                                },
+
+                            }}
+                        />
