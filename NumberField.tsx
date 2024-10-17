16d0f3e4435022aa9611c635d81c23fff7086fab
Thorax IT <118170367+thoraxit@users.noreply.github.com>
2024-10-18 03:57:50 +0600

    Update NumberField.tsx

--- NumberField.tsx
+++ NumberField.tsx
@@ -1,1 +1,9 @@
 // Number Field
+                        <TextField
+                            {...registerItem('stock',{ valueAsNumber: true })}
+                            label="Stock"
+                            error={!!errorsItem.stock}
+                            helperText={errorsItem.stock?.message}
+                            margin="normal"
+                            className="pr-2 w-[300px]"
+                        />
