64405ba42b02ea95b5f97d6e64dff25797e0a460
Thorax IT <118170367+thoraxit@users.noreply.github.com>
2024-10-18 04:03:02 +0600

    Create Autocomplete.tsx

---
+++ Autocomplete.tsx
@@ -0,0 +1,27 @@
+const [partyOptions, setPartyOptions] = useState<PartyOption[]>([]);
+const [party, setParty] = useState<PartyOption | null>(null);
+                   
+                    <Autocomplete
+                            className="pr-2 w-[300px]"
+                            sx={{ mt: 2 }}
+                            value={party}
+                            onChange={(event, newValue) => {
+                                setValue('partyId', newValue?.id ?? 0);
+                                setParty(newValue)
+                            }}
+
+                            options={partyOptions}
+                            getOptionLabel={(option) => option.name}  // Displays the name in the input field
+                            renderOption={(props, option) => (
+                                <li {...props} key={option.id}>
+                                    {option.name} (ID: {option.id})
+                                </li>
+                            )}
+                            isOptionEqualToValue={(option, value) => option.id === value.id} // Checks if the selected option matches
+                            renderInput={(params) =>
+                                <TextField {...params}
+                                    label="Select Party"
+                                    error={!!errors.partyId}
+                                    helperText={errors.partyId?.message}
+                                />}
+                        />
